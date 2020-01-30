49d40a9a874738b21b70efb3dbc8db156cad70c2
Igor Smirnov <piter3343@gmail.com>
2020-01-30 16:53:40 +0300

    empty classes added

---
+++ app/src/main/java/ru/spbhse/bingochgk/activities/CreateArticleActivity.kt
@@ -0,0 +1,13 @@
+package ru.spbhse.bingochgk.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.spbhse.bingochgk.R
+
+class CreateArticleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_article)
+    }
+}
---
+++ app/src/main/java/ru/spbhse/bingochgk/controller/ConcreteCollectionController.kt
@@ -0,0 +1,2 @@
+package ru.spbhse.bingochgk.controller
+
