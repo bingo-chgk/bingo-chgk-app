17f54168b47d532b4b86717429472646f02c5252
Igor Smirnov <piter3343@gmail.com>
2020-02-01 13:24:44 +0300

    implemented ui of article activity

--- Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/MainMenuActivity.kt
+++ Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/MainMenuActivity.kt
@@ -1,5 +1,6 @@
 package ru.spbhse.bingochgk.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.spbhse.bingochgk.R
@@ -9,5 +10,10 @@ class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        /*
+        // For testing ArticleActivity
+        startActivity(Intent(this, ArticleActivity::class.java))
+        */
     }
 }
