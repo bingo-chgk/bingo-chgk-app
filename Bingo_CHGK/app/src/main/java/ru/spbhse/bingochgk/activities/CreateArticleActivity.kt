04b433cb98f840cd222d0ac0e54409d42af6a54a
Darya Razumova <d.razumova@yandex.ru>
2020-02-10 16:54:13 +0300

    create topic activity

--- Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/CreateArticleActivity.kt
+++ Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/CreateArticleActivity.kt
@@ -3,11 +3,17 @@ package ru.spbhse.bingochgk.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.spbhse.bingochgk.R
+import kotlinx.android.synthetic.main.activity_create_article.*
 
 class CreateArticleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_article)
+
+        create_topic_button.setOnClickListener {
+            //TODO
+            finish()
+        }
     }
 }
