23e4a2ebf4b8174f429054f3f2eb185627f98085
Igor Smirnov <piter3343@gmail.com>
2020-02-12 19:36:07 +0300

    added popup menus

--- Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/ConcreteCollectionActivity.kt
+++ Bingo_CHGK/app/src/main/java/ru/spbhse/bingochgk/activities/ConcreteCollectionActivity.kt
@@ -3,8 +3,12 @@ package ru.spbhse.bingochgk.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_concrete_collection.*
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.activity_concrete_collection.add_topic_button
+import kotlinx.android.synthetic.main.activity_concrete_collection.toolbar
+import kotlinx.android.synthetic.main.activity_concrete_collection.topics_list
 import ru.spbhse.bingochgk.R
 import ru.spbhse.bingochgk.model.Topic
 
@@ -45,6 +49,10 @@ class ConcreteCollectionActivity : AppCompatActivity(), OnTopicClickListener {
     }
 
     override fun onItemLongClick(position: Int): Boolean {
+        val popupMenu = PopupMenu(this, topics_list[position])
+        popupMenu.menu.add("Удалить тему")
+        popupMenu.menu.add("Подгрузить вопросы по теме")
+        popupMenu.show()
         Toast.makeText(this, "long click ${initTopics[position].name}", Toast.LENGTH_LONG).show()
         return true
     }
